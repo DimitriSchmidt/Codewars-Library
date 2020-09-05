e3eb700b7995b09c9fb53c53402d0e1e4774440e
DimitriSchmidt <42524114+DimitriSchmidt@users.noreply.github.com>
2020-09-05 09:54:17 +0200

    Add files via upload
    You will be given a string and you task is to check if it is possible to convert that string into a palindrome by removing a single character. If the string is already a palindrome, return "OK". If it is not, and we can convert it to a palindrome by removing one character, then return "remove one", otherwise return "not possible". The order of the characters should not be changed.
    
    For example:
    solve("abba") = "OK". -- This is a palindrome
    solve("abbaa") = "remove one". -- remove the 'a' at the extreme right. 
    solve("abbaab") = "not possible".

---
+++ Palindrom.cs
@@ -0,0 +1,96 @@
+﻿
+using System.Diagnostics;
+
+
+namespace Palindrom
+{
+    class Program
+    {
+        public static int count { get; set; } = 0;
+
+        static void Main(string[] args)
+        {
+            
+
+            string s = "hannahx";
+            Debug.Print(s);
+            solve(s);
+        }
+
+
+        public static string solve(string s)
+        {
+           
+
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+
+                if (s[i] == s[s.Length - (i + 1)])
+                {
+                    //good news
+                }
+                else
+                {
+                 //bad news
+                 Debug.Print (Check(s));
+                 return Check(s);
+                }
+          
+            }
+
+            Debug.Print("OK");
+            return "OK";
+        }
+
+        public static string Check(string s)
+        {
+            string copy = s;
+
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                copy = s.Remove(i, 1);
+
+                if (Remove_check(copy))
+                {
+                    Debug.Print(copy);
+                    return "remove one";
+                }
+                else
+                {
+
+                }
+
+
+
+            }
+
+            Debug.Print(copy);
+            return "not possible";
+        }
+
+        public static bool Remove_check(string s)
+        {
+         
+
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+
+                if (s[i] == s[s.Length - (i + 1)])
+                {
+                    //good news
+                }
+                else
+                {
+                    //bad news
+                    return false;
+                 
+                }
+
+            }
+
+            Debug.Print("remove one");
+            return true;
+        }
+    }      
+}
