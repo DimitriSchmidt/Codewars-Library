4c9eba103f1082f5a0258c9feb10a20d88ec35c9
DimitriSchmidt <42524114+DimitriSchmidt@users.noreply.github.com>
2022-02-02 04:26:49 +0100

    Update and rename Program.cs to Deodorant Evaporator.cs

--- Deodorant Evaporator.cs
+++ Deodorant Evaporator.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace Codewars
 {
     class Program
@@ -16,16 +15,13 @@ namespace Codewars
                 double lose_ml_per_day = 0;
                 int count = 0;
                 double content_finaly =(threshold/100)*content;
-
+                
                 while(content > content_finaly)
                 {
                   lose_ml_per_day = (content / 100)*evap_per_day;
                   content-=lose_ml_per_day;
-                  
                   count++;
-                   
                 }
-            
                 return count;
 
             }
